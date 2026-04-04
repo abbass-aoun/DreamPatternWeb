260de433d9fa78cd454a238de18a3bd73bcb0b8f
abbass-aoun <abbass.aoun@icloud.com>
2026-04-04 18:36:33 +0300

    fix api base url

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,7 @@
 // ========================================
 // GLOBAL STATE & CONFIG
 // ========================================
-const API_BASE = 'http://localhost:5000/api';
+const API_BASE = '/api';
 let currentUser = null;
 
 // ========================================
